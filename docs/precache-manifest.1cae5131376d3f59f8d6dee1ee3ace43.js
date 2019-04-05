799e854dc7f7f4ce385b56627898a04184188027
Lucas Bassetti <lucasbr.dafonseca@gmail.com>
2019-04-05 13:35:34 -0300

    Add docs

---
+++ docs/precache-manifest.1cae5131376d3f59f8d6dee1ee3ace43.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0a99c63103b7956c6b9",
+    "url": "/static/js/runtime~main.adc9d4a6.js"
+  },
+  {
+    "revision": "f17ba7fcec654d857f0a",
+    "url": "/static/js/main.f7697d25.chunk.js"
+  },
+  {
+    "revision": "b84221822164d11dd0e5",
+    "url": "/static/js/4.d46654f0.chunk.js"
+  },
+  {
+    "revision": "72407dc08a387410cd2f",
+    "url": "/static/js/3.00adb416.chunk.js"
+  },
+  {
+    "revision": "4f299fd28d2ac0aa1567",
+    "url": "/static/js/2.85e3465b.chunk.js"
+  },
+  {
+    "revision": "f17ba7fcec654d857f0a",
+    "url": "/static/css/main.963bf418.chunk.css"
+  },
+  {
+    "revision": "b84221822164d11dd0e5",
+    "url": "/static/css/4.ae877ef3.chunk.css"
+  },
+  {
+    "revision": "4f299fd28d2ac0aa1567",
+    "url": "/static/css/2.964bb913.chunk.css"
+  },
+  {
+    "revision": "e2a5a1dc859b0d558811e7f51cde2c33",
+    "url": "/index.html"
+  }
+];
